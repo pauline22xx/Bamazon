9f751742f0bae05aaa70492f6cba20311ab37a57
Pauline Whang <pauline.s.wh@gmail.com>
2017-06-06 15:22:57 -0400

    finishing up 2

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -37,9 +37,51 @@ var promptPurchases = function() {
 	console.log('please select purchasing item and quantity');
 	prompt.start();
 	prompt.get(['itemID', 'quantity'], function(err, result) {
-
+		queryProduct(result);
 	});
 }
+
+var continueShopping = function() {
+	inquirer.prompt([{
+		type: 'confirm',
+		name: 'action',
+		message: 'continue shopping'
+	}]).then(function(res) {
+		if (!res.action) {
+			connection.end();
+			console.log('Thank you - come again');
+			return;
+		}else if (res.action) {
+			start();
+		}
+		});
+	}
+}
+
+var displayProducts = function() {
+	var t = new Table();
+
+	connection.query('SELECT * FROM products', function(error, results, fields) {
+		if (err) throw err;
+
+		results.forEach(function(product) {
+			t.cell('Product Id', product.item_id);
+			t.cell('Product Name', product.product_name);
+			t.cell('Pricem USD', product.price, Table.number(2));
+			t.newRow();
+		});
+		console.log('\n' + t.toString());
+	});
+	start();
+}
+
+var queryProduct = function(request) {
+	var productId = "'" + request.itemID + "'";
+	var sql = 'SELECT * FROM products WHERE item_id = ' + productId;
+}
+
+
+exports.start = start;
 
 
 
